195937d84da0b220ccfe087cdfc3500760327208
Rest <code.rest.dev@gmail.com>
2024-04-05 15:49:40 -0300

    0.1.8 Error Handling

--- server/controllers/store.controllers.js
+++ server/controllers/store.controllers.js
@@ -6,18 +6,8 @@ import router from "../routes/store.routes.js";
 // APARTADO CREAR EMPRESA
 
 export const createEmpresa = async (req, res) => {
-  const {
-    rutEmpresa,
-    nombreEmpresa,
-    descripcionEmpresa,
-    fundador,
-    nacionalidad,
-    imagen,
-    lema,
-  } = req.body;
-  const result = await pool.query(
-    "INSERT INTO empresa(rutEmpresa, nombreEmpresa, descripcionEmpresa, fundador, nacionalidad, imagen, lema) VALUES(?,?,?,?,?,?,?)",
-    [
+  try {
+    const {
       rutEmpresa,
       nombreEmpresa,
       descripcionEmpresa,
@@ -25,81 +15,99 @@ export const createEmpresa = async (req, res) => {
       nacionalidad,
       imagen,
       lema,
-    ]
-  );
-  console.log(result);
-  res.json({
-    rutEmpresa: result.rutEmpresa,
-    nombreEmpresa,
-    descripcionEmpresa,
-  });
+    } = req.body;
+    const result = await pool.query(
+      "INSERT INTO empresa(rutEmpresa, nombreEmpresa, descripcionEmpresa, fundador, nacionalidad, imagen, lema) VALUES(?,?,?,?,?,?,?)",
+      [
+        rutEmpresa,
+        nombreEmpresa,
+        descripcionEmpresa,
+        fundador,
+        nacionalidad,
+        imagen,
+        lema,
+      ]
+    );
+    console.log(result);
+    res.json({
+      rutEmpresa: result.rutEmpresa,
+      nombreEmpresa,
+      descripcionEmpresa,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 // APARTADO CREAR PRODUCTO
 
 export const createProducto = async (req, res) => {
-  const rutEmpresa = 211251026;
-  const {
-    nombreProducto,
-    descripcionProducto,
-    colorProducto,
-    imagen,
-    idCategoria,
-  } = req.body;
+  try {
+    const rutEmpresa = 211251026;
+    const {
+      nombreProducto,
+      descripcionProducto,
+      colorProducto,
+      imagen,
+      idCategoria,
+    } = req.body;
 
-  const result = pool.query(
-    "INSERT INTO productos(nombreProducto, descripcionProducto, colorProducto, imagen, rutEmpresa, idCategoria) VALUES(?,?,?,?,?,?)",
-    [
+    const result = pool.query(
+      "INSERT INTO productos(nombreProducto, descripcionProducto, colorProducto, imagen, rutEmpresa, idCategoria) VALUES(?,?,?,?,?,?)",
+      [
+        nombreProducto,
+        descripcionProducto,
+        colorProducto,
+        imagen,
+        rutEmpresa,
+        idCategoria,
+      ]
+    );
+    console.log(result);
+    res.json({
       nombreProducto,
       descripcionProducto,
       colorProducto,
       imagen,
       rutEmpresa,
       idCategoria,
-    ]
-  );
-  console.log(result);
-  res.json({
-    nombreProducto,
-    descripcionProducto,
-    colorProducto,
-    imagen,
-    rutEmpresa,
-    idCategoria,
-  });
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 // APARTADO CREAR CATEGORIA
 
 export const createCategoria = async (req, res) => {
-  const { nombreCategoria, descripcionCategoria, imagen } = req.body;
-  const result = pool.query(
-    "INSERT INTO categoria(nombreCategoria, descripcionCategoria, imagen) VALUES(?,?,?)",
-    [nombreCategoria, descripcionCategoria, imagen]
-  );
-  console.log(result);
-  res.json({
-    nombreCategoria,
-    descripcionCategoria,
-    imagen,
-  });
+  try {
+    const { nombreCategoria, descripcionCategoria, imagen } = req.body;
+    const result = pool.query(
+      "INSERT INTO categoria(nombreCategoria, descripcionCategoria, imagen) VALUES(?,?,?)",
+      [nombreCategoria, descripcionCategoria, imagen]
+    );
+    console.log(result);
+    res.json({
+      nombreCategoria,
+      descripcionCategoria,
+      imagen,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 // APARTADO CREAR INF NUTRICIONAL
 
 export const createInf = async (req, res) => {
-  const {
-    energia,
-    proteinas,
-    grasaTotal,
-    carbohidratos,
-    azucaresTotal,
-    sodio,
-    idProducto,
-  } = req.body;
-  const result = pool.query(
-    "INSERT INTO infNutricional(energia, proteinas, grasaTotal, carbohidratos, azucaresTotal, sodio, idProducto) VALUES(?,?,?,?,?,?,?)",
-    [
+  try {
+    const {
       energia,
       proteinas,
       grasaTotal,
@@ -107,47 +115,81 @@ export const createInf = async (req, res) => {
       azucaresTotal,
       sodio,
       idProducto,
-    ]
-  );
-  console.log(result);
-  res.json({
-    energia,
-    proteinas,
-    grasaTotal,
-    carbohidratos,
-    azucaresTotal,
-    sodio,
-    idProducto,
-  });
+    } = req.body;
+    const result = pool.query(
+      "INSERT INTO infNutricional(energia, proteinas, grasaTotal, carbohidratos, azucaresTotal, sodio, idProducto) VALUES(?,?,?,?,?,?,?)",
+      [
+        energia,
+        proteinas,
+        grasaTotal,
+        carbohidratos,
+        azucaresTotal,
+        sodio,
+        idProducto,
+      ]
+    );
+    console.log(result);
+    res.json({
+      energia,
+      proteinas,
+      grasaTotal,
+      carbohidratos,
+      azucaresTotal,
+      sodio,
+      idProducto,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 //-------------------------------------- METODO GET -----------------------------------------------
 
 export const getEmpresa = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM empresa");
-  res.json(result[0]);
+  try {
+    const [result] = await pool.query("SELECT * FROM empresa");
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 export const getProductos = async (req, res) => {
-  const [result] = await pool.query("SELECT * FROM productos");
-  res.json(result);
+  try {
+    const [result] = await pool.query("SELECT * FROM productos");
+    res.json(result);
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 export const getProducto = async (req, res) => {
-  // QUERY PARA EL PRODUCTO POR ID y PARA LA INFORMACION NUTRICIONAL POR ID
-  const [result] = await pool.query(
-    "SELECT * FROM productos WHERE idProducto = ? ",
-    [req.params.id]
-  );
-  const [result2] = await pool.query(
-    "SELECT * FROM infNutricional WHERE idInfNutricional = ? ",
-    [req.params.id]
-  );
-  if (result.length >= 1) {
-    res.json(result.concat(result2));
-  } else {
-    res.json({
-      message: "No se encontro el producto deseado",
+  try {
+    // QUERY PARA EL PRODUCTO POR ID y PARA LA INFORMACION NUTRICIONAL POR ID
+    const [result] = await pool.query(
+      "SELECT * FROM productos WHERE idProducto = ? ",
+      [req.params.id]
+    );
+    const [result2] = await pool.query(
+      "SELECT * FROM infNutricional WHERE idInfNutricional = ? ",
+      [req.params.id]
+    );
+    if (result.length >= 1) {
+      res.json(result.concat(result2));
+    } else {
+      res.json({
+        message: "No se encontro el producto deseado",
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
     });
   }
 };
@@ -156,18 +198,24 @@ export const getProducto = async (req, res) => {
 //------------------------------------ METODO DELETE ----------------------------------------------
 
 export const deleteProducto = async (req, res) => {
-  console.log(req.params.id);
-  const [result] = await pool.query(
-    "DELETE FROM productos WHERE idProducto = ?",
-    [req.params.id]
-  );
-  if (result.affectedRows === 0) {
-    return res
-      .status(404)
-      .json({ message: "Producto no encontrado en la base de datos" });
-  }
+  try {
+    console.log(req.params.id);
+    const [result] = await pool.query(
+      "DELETE FROM productos WHERE idProducto = ?",
+      [req.params.id]
+    );
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ message: "Producto no encontrado en la base de datos" });
+    }
 
-  res.sendStatus(204);
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
 //-------------------------------------------------------------------------------------------------
@@ -175,31 +223,47 @@ export const deleteProducto = async (req, res) => {
 //------------------------------------- METODO PUT ------------------------------------------------
 
 export const updateProducto = async (req, res) => {
-  const [result] = await pool.query(
-    "UPDATE productos SET ? WHERE idProducto = ?",
-    [req.body, req.params.id]
-  );
+  try {
+    const [result] = await pool.query(
+      "UPDATE productos SET ? WHERE idProducto = ?",
+      [req.body, req.params.id]
+    );
 
-  res.json(result);
+    res.json(result);
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
 };
 
+export const updateInfNutricional = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "UPDATE infNutricional SET ? WHERE idInfNutricional = ?",
+      [req.body, req.params.id]
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
-export const updateInfNutricional = async (req, res) =>{
-  const [result] = await pool.query("UPDATE infNutricional SET ? WHERE idInfNutricional = ?", [
-    req.body,
-    req.params.id
-  ]);
-  res.json(result)
-}
+export const updateCategoria = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "UPDATE categoria SET ? WHERE idCategoria = ?",
+      [req.body, req.params.id]
+    );
 
-
-export const updateCategoria = async (req, res) =>{
-  const [result] = await pool.query("UPDATE categoria SET ? WHERE idCategoria = ?", [
-    req.body,
-    req.params.id
-  ])
-
-  res.json(result)
-}
+    res.json(result);
+  } catch (error) {
+    return res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
 //-------------------------------------------------------------------------------------------------
