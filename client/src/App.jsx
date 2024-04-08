60d23fea51785c896f7eaaef3eff57f6ac33a28b
Rest <code.rest.dev@gmail.com>
2024-04-07 23:10:21 -0400

    0.2.1 New route CreateProduct

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
-import ProductsList from './pages/ProductsList'
+import ProductsList from './pages/ProductsPage'
+import ProductForm from './pages/ProductForm'
 
 function App() {
   return (
     <Routes>
       <Route path = "/" element = {<ProductsList/>} ></Route>
+      <Route path = "/newProduct" element={<ProductForm/>}></Route>
     </Routes>
   )
 }
---
+++ client/src/pages/ProductForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductForn() {
+  return (
+    <div>Crear formulario ProductForn</div>
+  )
+}
+
+export default ProductForn
