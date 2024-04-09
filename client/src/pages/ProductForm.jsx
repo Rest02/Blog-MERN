1282e5b3c7491af1d979ccc8c4a901074761e64e
Rest <code.rest.dev@gmail.com>
2024-04-08 22:42:15 -0400

    0.2.5 Form , 1ra parte

--- client/src/pages/ProductForm.jsx
+++ client/src/pages/ProductForm.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Form, Formik } from "formik";
 
 function ProductForm() {
   return (
-    <div>Crear formulario ProductForn</div>
-  )
+    <div>
+      <Formik
+        initialValues={{
+          Nombre: "",
+          Descripción: "",
+          Color: "",
+        }}
+      >
+        {({handleChange}) => (
+          <Form>
+            {/* --------------------------------------------------------------------------------- */}
+            <label>Nombre</label>
+            <input
+              type="text"
+              name="Nombre"
+              placeholder="Escribe le nombre del producto"
+            />
+            {/* --------------------------------------------------------------------------------- */}
+            <label>Descripción</label>
+            <textarea
+              name="Descripción"
+              cols="30"
+              rows="10"
+              placeholder="Escribe una descripción del producto"
+            ></textarea>
+            {/* --------------------------------------------------------------------------------- */}
+            <label>Color</label>
+            <input
+              type="text"
+              name="Color"
+              placeholder="Ingresa el color principal del producto"
+            />
+            {/* --------------------------------------------------------------------------------- */}
+
+            <button>Save</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
-export default ProductForm
+export default ProductForm;
