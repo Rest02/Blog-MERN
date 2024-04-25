60c162f197185df594bc29a5d61c8cd0bf42a360
Rest <code.rest.dev@gmail.com>
2024-04-25 15:23:54 -0400

    0.4.5 Agregandole funcionalidad al boton de editar

--- client/src/components/ProductoCard.jsx
+++ client/src/components/ProductoCard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function ProductoCard({producto}) {
+function ProductoCard({ producto }) {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>{producto.nombreProducto}</h1>
       <p>{producto.descripcionProducto}</p>
       <img src={`http://localhost:4000/images/` + producto.imagen} alt="" />
-      <button>Editar</button>
+      <button onClick={() => navigate(`/editProduct/${producto.idProducto}`)}>
+        Editar
+      </button>
       <button>Eliminar</button>
     </div>
   );
