36b417253e31ddc53f4bf86bc27a1d86e546b62f
Rest <code.rest.dev@gmail.com>
2024-05-02 18:47:16 -0400

    0.6.1 Listando las características y propiedades del producto

--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useInfood } from "../Context/Context";
 
 function ViewProductPage() {
+  const params = useParams();
+  const { getOneProducto } = useInfood();
+
+  useEffect(() => {
+    async function listOneProduct() {
+      const response = await getOneProducto(params.id);
+      setProductos(response);
+    }
+    listOneProduct();
+  });
+
+  const [producto, setProductos] = useState([]);
+
   return (
-    <div>ViewProductPage</div>
-  )
+    <div>
+      {producto.map((pro) => (
+        <div key={pro.idProducto}>
+          <h1>{pro.nombreProducto}</h1>
+          <h2>{pro.descripcionProducto}</h2>
+        <p>{pro.colorProducto}</p>
+          <img src={`http://localhost:4000/images/` + pro.imagen} alt="" />
+
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default ViewProductPage
+export default ViewProductPage;
