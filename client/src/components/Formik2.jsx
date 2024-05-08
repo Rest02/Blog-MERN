afc98da84fa825ecc0e90d58d5a6ff1e0d9f7f97
Rest <code.rest.dev@gmail.com>
2024-05-07 23:54:44 -0400

    0.7.1 Cambiando formulario

--- client/src/components/Formik2.jsx
+++ client/src/components/Formik2.jsx
@@ -2,7 +2,75 @@ import React from 'react'
 
 function Formik2() {
   return (
-    <div>Formik2</div>
+    <div>Informacion nutricional</div>
+
+
+            // {/* <h1>Información Nutricional</h1>
+            // <label>Energia</label>
+            // <input
+            //   type="number"
+            //   name="Energia"
+            //   placeholder="Ingresar un numero referente a la energia del producto"
+            //   onChange={handleChange}
+            //   value={values.Energia}
+            // /> */}
+
+            // {/* --------------------------------------------------------------------------------- */}
+
+            {/* <label>Proteinas</label>
+            <input
+              type="number"
+              name="proteinas"
+              placeholder="Ingresar un numero referente a la proteina del producto"
+              onChange={handleChange}
+              value={values.proteinas}
+            /> */}
+
+            {/* --------------------------------------------------------------------------------- */}
+
+            {/* <label>Grasa Total</label>
+            <input
+              type="number"
+              name="grasaTotal"
+              placeholder="Ingresar un numero referente a la Grasa total del producto"
+              onChange={handleChange}
+              value={values.grasaTotal}
+            /> */}
+
+            {/* --------------------------------------------------------------------------------- */}
+
+            {/* <label>Carbohidratos</label>
+            <input
+              type="number"
+              name="Carbohidratos"
+              placeholder="Ingresar un numero referente a los carbohidratos totales del producto"
+              onChange={handleChange}
+              value={values.Carbohidratos}
+            /> */}
+
+            {/* --------------------------------------------------------------------------------- */}
+            
+            {/* <label>Azucares Totales</label>
+            <input
+              type="number"
+              name="azucaresTotal"
+              placeholder="Ingresar un numero referente a los azucares totales del producto"
+              onChange={handleChange}
+              value={values.azucaresTotal}
+            /> */}
+
+            {/* --------------------------------------------------------------------------------- */}
+
+            {/* <label>Sodio</label>
+            <input
+              type="number"
+              name="sodio"
+              placeholder="Ingresar un numero referente a el sodio total del producto"
+              onChange={handleChange}
+              value={values.sodio}
+            /> */}
+
+            {/* --------------------------------------------------------------------------------- */}
   )
 }
 
--- client/src/pages/ProductForm.jsx
+++ client/src/pages/ProductForm.jsx
@@ -115,74 +115,7 @@ function ProductForm() {
               <option value="3">Categoria 3</option>
             </select>
 
-            {/* --------------------------------------------------------------------------------- */}
 
-            {/* <h1>Información Nutricional</h1>
-            <label>Energia</label>
-            <input
-              type="number"
-              name="Energia"
-              placeholder="Ingresar un numero referente a la energia del producto"
-              onChange={handleChange}
-              value={values.Energia}
-            /> */}
-
-            {/* --------------------------------------------------------------------------------- */}
-
-            {/* <label>Proteinas</label>
-            <input
-              type="number"
-              name="proteinas"
-              placeholder="Ingresar un numero referente a la proteina del producto"
-              onChange={handleChange}
-              value={values.proteinas}
-            /> */}
-
-            {/* --------------------------------------------------------------------------------- */}
-
-            {/* <label>Grasa Total</label>
-            <input
-              type="number"
-              name="grasaTotal"
-              placeholder="Ingresar un numero referente a la Grasa total del producto"
-              onChange={handleChange}
-              value={values.grasaTotal}
-            /> */}
-
-            {/* --------------------------------------------------------------------------------- */}
-
-            {/* <label>Carbohidratos</label>
-            <input
-              type="number"
-              name="Carbohidratos"
-              placeholder="Ingresar un numero referente a los carbohidratos totales del producto"
-              onChange={handleChange}
-              value={values.Carbohidratos}
-            /> */}
-
-            {/* --------------------------------------------------------------------------------- */}
-            
-            {/* <label>Azucares Totales</label>
-            <input
-              type="number"
-              name="azucaresTotal"
-              placeholder="Ingresar un numero referente a los azucares totales del producto"
-              onChange={handleChange}
-              value={values.azucaresTotal}
-            /> */}
-
-            {/* --------------------------------------------------------------------------------- */}
-
-            {/* <label>Sodio</label>
-            <input
-              type="number"
-              name="sodio"
-              placeholder="Ingresar un numero referente a el sodio total del producto"
-              onChange={handleChange}
-              value={values.sodio}
-            /> */}
-
-            {/* --------------------------------------------------------------------------------- */}
 
             <button type="submit" disabled={isSubmitting}>
               {isSubmitting ? "Saving..." : "Save"}
