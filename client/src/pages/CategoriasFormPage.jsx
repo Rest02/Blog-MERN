f26011f455e9cde3d4f8acd8bce70eb7fcbf3b5e
Rest <code.rest.dev@gmail.com>
2024-05-16 22:54:30 -0400

    0.9.6 Creando Formulario para el POST de Categorias

--- client/src/pages/CategoriasFormPage.jsx
+++ client/src/pages/CategoriasFormPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form } from "formik";
 
 function CategoriasFormPage() {
   return (
-    <div>CategoriasFormPage</div>
-  )
+    <div>
+      <Formik
+        initialValues={{
+          nombreCategoria: "",
+          descripcionCategoria: "",
+          imagen: "",
+        }}
+
+        onSubmit={(values)=>{
+            console.log(values)
+        }}
+      >
+        {({ handleChange, handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <label>Nombre de la categoria: </label>
+            <input type="text" name="nombreCategoria" onChange={handleChange}/>
+
+            <label>Descripciópn de la categoria: </label>
+            <input type="text" name="descripcionCategoria" onChange={handleChange}/>
+
+            <label>Imagen de la categoria: </label>
+            <input type="file" name="imagen" onChange={handleChange}/>
+
+            <button type="submit">Save</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
-export default CategoriasFormPage
+export default CategoriasFormPage;
