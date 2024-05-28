04944ef9ebf63ca71aa96dcad073ba128c799036
Rest <code.rest.dev@gmail.com>
2024-05-27 23:37:02 -0400

    1.1.8 Avance de formulario

--- client/src/pages/ComparativaPage.jsx
+++ client/src/pages/ComparativaPage.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useInfood } from "../Context/Context";
 import { Formik, Form } from "formik";
 
-
 function ComparativaPage() {
+  // const { getOneProductoIndv, producto } = useInfood();
+
   return (
     <div>
-      <Formik>
+      <Formik
+        initialValues={{
+          producto1: "",
+          producto2: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(handleChange, handleSubmit) => (
+          <Form onSubmit={handleSubmit}>
+            <label>Seleccione alternativa A</label>
+            <select name="producto1" onChange={handleChange}>
+              <option>hola2</option>
+            </select>
+            <label>Seleccione producto B</label>
+            <select name="producto2" onChange={handleChange}>
+              <option>hola1</option>
+            </select>
 
-        <Form>
-
-          <label>Seleccione alternativa A</label>
-          <select name="" id=""></select>
-          <label>Seleccione producto B</label>
-          <select name="" id=""></select>
-
-
-
-        </Form>
-
+            <button type="submit">save</button>
+          </Form>
+        )}
       </Formik>
     </div>
-  )
+  );
 }
 
-export default ComparativaPage
+export default ComparativaPage;
