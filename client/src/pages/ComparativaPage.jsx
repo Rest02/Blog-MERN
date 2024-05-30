656350203e77ae5f82481f2715576a00c811744e
Rest <code.rest.dev@gmail.com>
2024-05-29 23:24:10 -0400

    1.2.0 Listando los productos como opciones

--- client/src/pages/ComparativaPage.jsx
+++ client/src/pages/ComparativaPage.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useInfood } from "../Context/Context";
 import { Formik, Form } from "formik";
 
 function ComparativaPage() {
-  const { getOneProductoIndv, producto } = useInfood();
+  const { cargarTareas, productos } = useInfood();
+
+
+  useEffect(()=>{
+    async function loadProducts(){
+      await cargarTareas()
+    }
+    loadProducts()
+  },[])
 
   return (
     <div>
@@ -12,8 +20,8 @@ function ComparativaPage() {
           producto1: "",
           producto2: "",
         }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values) => {
+          console.log(values)
         }}
       >
         {(
@@ -22,13 +30,15 @@ function ComparativaPage() {
           <Form onSubmit={handleSubmit}>
             <label>Seleccione alternativa A</label>
             <select name="producto1" onChange={handleChange}>
-              <option value="hola2">hola2</option>
-              <option value="hola3">hola3</option>
+              {productos.map((cat)=>(
+                <option key={cat.idProducto} value={cat.idProducto}>{cat.nombreProducto}</option>
+              ))}
             </select>
             <label>Seleccione producto B</label>
             <select name="producto2" onChange={handleChange}>
-              <option value="hola1">hola1</option>
-              <option value="hola4">hola4</option>
+              {productos.map((cat)=>(
+                <option key={cat.idProducto} value={cat.idProducto}>{cat.nombreProducto}</option>
+              ))}
             </select>
 
             <button type="submit">save</button>
