75711472e05f44bb8ebccc0a92e1fcc7de9e466d
Rest <code.rest.dev@gmail.com>
2024-06-26 23:32:16 -0400

    1.5.2 Agregando componente de categorias

--- client/src/components/CategoriaCard.jsx
+++ client/src/components/CategoriaCard.jsx
@@ -1,25 +1,57 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'
-import { useInfood } from '../Context/Context'
+import { useNavigate } from "react-router-dom";
+import { useInfood } from "../Context/Context";
 
 function CategoriaCard({ categoria }) {
-
-  const navigate = useNavigate()
-
-  const {deleteCategoria} = useInfood()
+  const navigate = useNavigate();
+  const { deleteCategoria } = useInfood();
 
   return (
-    <div>
-      <h1>Categorias</h1>
-      {categoria.map((cat) => (
-        <div key={cat.idCategoria}>
-          <h1>{cat.nombreCategoria}</h1>
-          <p>{cat.descripcionCategoria}</p>
-          <img src={`http://localhost:4000/images/` + cat.imagen} alt="" />
-          <button onClick={()=>(navigate(`/categoriaForm/${cat.idCategoria}`))}>Editar Categoria</button>
-          <button onClick={()=>(deleteCategoria(cat.idCategoria) && navigate(0))}>Eliminar Categoria</button>
-        </div>
-      ))}
+    <div className="min-w-screen min-h-screen flex items-center p-5 lg:p-10 overflow-hidden relative">
+      <div className="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left">
+        <h1 className="font-bold text-3xl mb-10 text-center">Categorías</h1>
+        {categoria.map((cat) => (
+          <div className="md:flex items-center -mx-10 border border-black mb-10" key={cat.idCategoria}>
+            <div className="w-full md:w-1/2 px-10 mb-10 md:mb-0">
+              <div className="relative">
+                <img
+                  src={`http://localhost:4000/images/${cat.imagen}`}
+                  className="w-full relative z-10"
+                  alt={cat.nombreCategoria}
+                />
+                <div className="border border-black absolute top-10 bottom-10 left-10 right-10 z-0"></div>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 px-10">
+              <div className="mb-10">
+                <h1 className="font-bold uppercase text-2xl mb-5">
+                  {cat.nombreCategoria}
+                </h1>
+                <p className="text-sm mb-5">
+                  {cat.descripcionCategoria}
+                </p>
+              </div>
+              <div>
+                <button
+                  onClick={() => navigate(`/categoriaForm/${cat.idCategoria}`)}
+                  className="bg-blue-500 text-white rounded-full px-5 py-2 mr-5 hover:bg-blue-700"
+                >
+                  Editar Categoría
+                </button>
+                <button
+                  onClick={() => {
+                    deleteCategoria(cat.idCategoria);
+                    navigate(0);
+                  }}
+                  className="bg-red-500 text-white rounded-full px-5 py-2 hover:bg-red-700"
+                >
+                  Eliminar Categoría
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
