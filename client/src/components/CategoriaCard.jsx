4402d1a0eac11928a9a1c9d040ea637a3e0229b1
Rest <code.rest.dev@gmail.com>
2024-07-01 23:55:11 -0400

    1.5.7 Mejorando componente de categorias

--- client/src/components/CategoriaCard.jsx
+++ client/src/components/CategoriaCard.jsx
@@ -7,46 +7,52 @@ function CategoriaCard({ categoria }) {
   const { deleteCategoria } = useInfood();
 
   return (
-    <div className="flex justify-center items-center">
-      <div className="2xl:mx-auto 2xl:container py-12 px-4 sm:px-6 xl:px-20 2xl:px-0 w-full">
-        <div className="flex flex-col justify-center items-center space-y-10">
-          <div className="flex flex-col justify-center items-center ">
-            <h1 className="text-3xl xl:text-4xl font-semibold leading-7 xl:leading-9 text-gray-800 dark:text-white">Categorías</h1>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full">
-            {categoria.map((cat) => (
-              <div className="relative group flex justify-center items-center h-full w-full" key={cat.idCategoria}>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {categoria.map((cat) => (
+            <div className="p-4 md:w-1/3" key={cat.idCategoria}>
+              <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
                 <img
-                  className="object-center object-cover h-full w-full"
+                  className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
                   src={`http://localhost:4000/images/${cat.imagen}`}
                   alt={cat.nombreCategoria}
                 />
-                <div className="absolute bottom-4 z-10 flex flex-col items-center">
-                  <button
-                    onClick={() => navigate(`/categoriaForm/${cat.idCategoria}`)}
-                    className="dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white mb-2"
-                  >
-                    Editar
-                  </button>
-                  <button
-                    onClick={() => {
-                      deleteCategoria(cat.idCategoria);
-                      navigate(0);
-                    }}
-                    className="dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 text-base font-medium leading-none text-gray-800 py-3 w-36 bg-white"
-                  >
-                    Eliminar
-                  </button>
+                <div className="p-6">
+                  <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                    {cat.nombreCategoria}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-gray-600 mb-3">
+                    {cat.nombreCategoria}
+                  </h1>
+                  <p className="leading-relaxed mb-3">
+                    {cat.descripcion}
+                  </p>
+                  <div className="flex items-center flex-wrap space-x-3">
+                    <button
+                      onClick={() => navigate(`/categoriaForm/${cat.idCategoria}`)}
+                      className="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md shadow-cla-blue px-4 py-1 rounded-lg"
+                    >
+                      Editar
+                    </button>
+                    <button
+                      onClick={() => {
+                        deleteCategoria(cat.idCategoria);
+                        navigate(0);
+                      }}
+                      className="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md shadow-cla-blue px-4 py-1 rounded-lg"
+                    >
+                      Eliminar
+                    </button>
+                  </div>
                 </div>
-                <div className="absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50"></div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
 export default CategoriaCard;
-
