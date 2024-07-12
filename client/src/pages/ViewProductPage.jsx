a330933ac99144baa37d70fae5f9047c0e2260c3
Rest <code.rest.dev@gmail.com>
2024-07-11 23:58:02 -0400

    1.6.5 nuevo comit

--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useInfood } from "../Context/Context";
 import CardComponent1 from "../components/CardComponent1";
@@ -25,21 +25,25 @@ function ViewProductPage() {
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8">
       <CardComponent1 producto={producto} />
       {infNutricional && infNutricional.length > 0 ? (
-        <div>
-          <InfNutricionalCard infNutricional = {infNutricional}/>
+        <div className="mt-4">
+          <InfNutricionalCard infNutricional={infNutricional} />
           <button
             onClick={() => navigate(`/EditarinfNutricional/${params.id}`)}
+            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
-            Editar Informacion Nutricional
+            Editar Información Nutricional
           </button>
         </div>
       ) : (
-        <div>
-          <button onClick={() => navigate(`/infNutricional/${params.id}`)}>
-            Crear InfNutricional
+        <div className="mt-4">
+          <button
+            onClick={() => navigate(`/infNutricional/${params.id}`)}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Crear Información Nutricional
           </button>
         </div>
       )}
