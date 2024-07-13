ce1b2d78d73bb308534aa839f89e6a20bdbbeae1
Rest <code.rest.dev@gmail.com>
2024-07-12 21:39:13 -0400

    1.6.6 Arreglando detalles

--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -8,8 +8,7 @@ import InfNutricionalCard from "../components/InfNutricionalCard";
 function ViewProductPage() {
   const navigate = useNavigate();
   const params = useParams();
-  const { getOneProductoIndv, producto, getOneInfNutricional, infNutricional } =
-    useInfood();
+  const { getOneProductoIndv, producto, getOneInfNutricional, infNutricional } = useInfood();
 
   useEffect(() => {
     async function listOneProduct() {
@@ -22,7 +21,7 @@ function ViewProductPage() {
 
     listOneProduct();
     listInfNutricional();
-  }, []);
+  }, [params.id, getOneProductoIndv, getOneInfNutricional]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8">
@@ -32,7 +31,7 @@ function ViewProductPage() {
           <InfNutricionalCard infNutricional={infNutricional} />
           <button
             onClick={() => navigate(`/EditarinfNutricional/${params.id}`)}
-            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="mt-4 transition-colors duration-500 bg-green-600 hover:bg-green-700 border border-black text-white px-4 py-2 rounded-lg"
           >
             Editar Información Nutricional
           </button>
@@ -41,7 +40,7 @@ function ViewProductPage() {
         <div className="mt-4">
           <button
             onClick={() => navigate(`/infNutricional/${params.id}`)}
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="transition-colors duration-500 bg-green-600 hover:bg-green-700 border border-black text-white px-4 py-2 rounded-lg"
           >
             Crear Información Nutricional
           </button>
