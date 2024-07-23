bf0d0066f3e67477d441be4172e4243dc50ec058
Rest <code.rest.dev@gmail.com>
2024-07-22 22:24:05 -0400

    1.7.1 Arreglando detalles de inf nutricional

--- client/src/components/CardComponent1.jsx
+++ client/src/components/CardComponent1.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function CardComponent1({ producto }) {
+function CardComponent1({ producto, infNutricional }) {
   return (
     <div className="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left border border-black">
       {producto.map((pro) => (
@@ -49,9 +49,48 @@ function CardComponent1({ producto }) {
                   <i className="mdi mdi-cart -ml-2 mr-2"></i> BUY NOW
                 </button>
               </div>
+              <div className="min-h-screen flex items-center justify-center px-4">
+      <div className="w-full max-w-6xl bg-white rounded-lg shadow-xl border border-black">
+        <div className="p-4 border-b border-black">
+          <h2 className="text-2xl">Información Nutricional</h2>
+          <p className="text-sm text-gray-500">Detalles nutricionales del producto</p>
+        </div>
+        <div>
+          {infNutricional.map((inf) => (
+            <div key={inf.idInfNutricional}>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                <p className="text-gray-600 md:col-span-1">Energía del producto</p>
+                <p className="md:col-span-1">{inf.energia}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                <p className="text-gray-600 md:col-span-1">Proteínas del producto</p>
+                <p className="md:col-span-1">{inf.proteinas}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                <p className="text-gray-600 md:col-span-1">Grasa Total del producto</p>
+                <p className="md:col-span-1">{inf.grasaTotal}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                <p className="text-gray-600 md:col-span-1">Carbohidratos Totales del producto</p>
+                <p className="md:col-span-1">{inf.carbohidratos}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                <p className="text-gray-600 md:col-span-1">Azúcares Totales del producto</p>
+                <p className="md:col-span-1">{inf.azucaresTotal}</p>
+              </div>
+              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                <p className="text-gray-600 md:col-span-1">Sodio Total del producto</p>
+                <p className="md:col-span-1">{inf.sodio}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
             </div>
           </div>
         </div>
+        
       ))}
     </div>
   );
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -25,10 +25,11 @@ function ViewProductPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-8">
-      <CardComponent1 producto={producto} />
+      
       {infNutricional && infNutricional.length > 0 ? (
         <div className="mt-4">
-          <InfNutricionalCard infNutricional={infNutricional} />
+          <CardComponent1 producto={producto} infNutricional={infNutricional}/>
+          {/* <InfNutricionalCard infNutricional={infNutricional} /> */}
           <button
             onClick={() => navigate(`/EditarinfNutricional/${params.id}`)}
             className="mt-4 transition-colors duration-500 bg-green-600 hover:bg-green-700 border border-black text-white px-4 py-2 rounded-lg"
