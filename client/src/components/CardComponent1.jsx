e6f6e42c129bec2da601d9207240cd675cea5575
Rest <code.rest.dev@gmail.com>
2024-07-23 20:47:58 -0400

    1.7.2 Posicionando bien la tabla de inf nutricional

--- client/src/components/CardComponent1.jsx
+++ client/src/components/CardComponent1.jsx
@@ -4,10 +4,7 @@ function CardComponent1({ producto, infNutricional }) {
   return (
     <div className="w-full max-w-6xl rounded bg-white shadow-xl p-10 lg:p-20 mx-auto text-gray-800 relative md:text-left border border-black">
       {producto.map((pro) => (
-        <div
-          className="md:flex items-center -mx-10"
-          key={pro.idProducto}
-        >
+        <div className="md:flex items-center -mx-10" key={pro.idProducto}>
           <div className="w-full md:w-1/2 px-10 mb-10 md:mb-0">
             <div className="relative">
               <img
@@ -49,51 +46,53 @@ function CardComponent1({ producto, infNutricional }) {
                   <i className="mdi mdi-cart -ml-2 mr-2"></i> BUY NOW
                 </button>
               </div>
-              <div className="min-h-screen flex items-center justify-center px-4">
-      <div className="w-full max-w-6xl bg-white rounded-lg shadow-xl border border-black">
-        <div className="p-4 border-b border-black">
-          <h2 className="text-2xl">Información Nutricional</h2>
-          <p className="text-sm text-gray-500">Detalles nutricionales del producto</p>
-        </div>
-        <div>
-          {infNutricional.map((inf) => (
-            <div key={inf.idInfNutricional}>
-              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
-                <p className="text-gray-600 md:col-span-1">Energía del producto</p>
-                <p className="md:col-span-1">{inf.energia}</p>
-              </div>
-              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
-                <p className="text-gray-600 md:col-span-1">Proteínas del producto</p>
-                <p className="md:col-span-1">{inf.proteinas}</p>
-              </div>
-              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
-                <p className="text-gray-600 md:col-span-1">Grasa Total del producto</p>
-                <p className="md:col-span-1">{inf.grasaTotal}</p>
-              </div>
-              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
-                <p className="text-gray-600 md:col-span-1">Carbohidratos Totales del producto</p>
-                <p className="md:col-span-1">{inf.carbohidratos}</p>
-              </div>
-              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
-                <p className="text-gray-600 md:col-span-1">Azúcares Totales del producto</p>
-                <p className="md:col-span-1">{inf.azucaresTotal}</p>
-              </div>
-              <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
-                <p className="text-gray-600 md:col-span-1">Sodio Total del producto</p>
-                <p className="md:col-span-1">{inf.sodio}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
             </div>
           </div>
         </div>
-        
       ))}
+      <div className="flex items-center justify-center px-4 mt-10">
+        <div className="w-full max-w-6xl bg-white rounded-lg shadow-xl border border-black">
+          <div className="p-4 border-b border-black">
+            <h2 className="text-2xl">Información Nutricional</h2>
+            <p className="text-sm text-gray-500">
+              Detalles nutricionales del producto
+            </p>
+          </div>
+          <div>
+            {infNutricional.map((inf) => (
+              <div key={inf.idInfNutricional}>
+                <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                  <p className="text-gray-600 md:col-span-1">Energía del producto</p>
+                  <p className="md:col-span-1">{inf.energia}</p>
+                </div>
+                <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                  <p className="text-gray-600 md:col-span-1">Proteínas del producto</p>
+                  <p className="md:col-span-1">{inf.proteinas}</p>
+                </div>
+                <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                  <p className="text-gray-600 md:col-span-1">Grasa Total del producto</p>
+                  <p className="md:col-span-1">{inf.grasaTotal}</p>
+                </div>
+                <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                  <p className="text-gray-600 md:col-span-1">Carbohidratos Totales del producto</p>
+                  <p className="md:col-span-1">{inf.carbohidratos}</p>
+                </div>
+                <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                  <p className="text-gray-600 md:col-span-1">Azúcares Totales del producto</p>
+                  <p className="md:col-span-1">{inf.azucaresTotal}</p>
+                </div>
+                <div className="md:grid md:grid-cols-2 hover:bg-gray-50 md:space-y-0 space-y-1 p-6 border-b border-black">
+                  <p className="text-gray-600 md:col-span-1">Sodio Total del producto</p>
+                  <p className="md:col-span-1">{inf.sodio}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default CardComponent1;
+
